946f92ba80177872f2a97f94c9665b00644995e6
Nobody1902 <noboy1902@proton.me>
2026-07-18 23:12:52 +0200

    Add search suggestions.

--- src/zymusic/views/search.py
+++ src/zymusic/views/search.py
@@ -16,6 +16,42 @@ class SearchView(ft.Column):
         self._player = player
         super().__post_init__(ref)
 
+    async def _on_suggestion_click(self, e):
+        suggestion = e.control.data
+        self.search_box.value = suggestion
+        await self.search_box.close_view()
+        self._search()
+
+    async def _fetch_suggestions(self, query: str):
+        suggestions = self._player.search_suggestions(query)
+        if suggestions:
+            self.search_box.controls = [
+                ft.ListTile(
+                    leading=ft.Icon(ft.Icons.SEARCH),
+                    title=ft.Text(s),
+                    data=s,
+                    on_click=self._on_suggestion_click,
+                )
+                for s in suggestions
+            ]
+        else:
+            await self.search_box.close_view()
+
+    async def _on_change(self, e):
+        query = e.control.value.strip()
+        if not query:
+            self.search_box.controls = []
+            return
+
+        await self._fetch_suggestions(query)
+
+    async def _on_submit(self, _):
+        await self.search_box.close_view()
+        self._search()
+
+    async def _on_tap(self, _):
+        await self.search_box.open_view()
+
     def _search(self):
         query = self.search_box.value.strip()
         if not query:
@@ -38,22 +74,25 @@ class SearchView(ft.Column):
     def init(self):
         super().init()
 
-        self.search_box = ft.TextField(
-            hint_text="Search...",
-            prefix_icon=ft.Icons.SEARCH,
-            border_radius=12,
-            expand=True,
-            autofocus=True,
-            height=50,
+        self.search_box = ft.SearchBar(
+            bar_hint_text="Search...",
+            view_hint_text="Search songs...",
+            bar_leading=ft.Icon(ft.Icons.SEARCH),
             keyboard_type=ft.KeyboardType.WEB_SEARCH,
-            on_submit=self._search,
+            autofocus=True,
+            shrink_wrap=True,
+            divider_color=ft.Colors.PRIMARY,
+            on_change=self._on_change,
+            on_submit=self._on_submit,
+            on_tap=self._on_tap,
+            expand=True,
         )
 
         self.search_results = ft.ListView(expand=True, spacing=10)
 
         self.controls = [
             ft.Row(
-                height=50,
+                height=70,
                 controls=[
                     self.search_box,
                 ],
